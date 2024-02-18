e946b0b277351a56916d5967458c85dcdfdbba8c
firuz-macbook <razackoff@gmail.com>
2024-02-18 22:28:57 +0200

    Add Authorization ans DataBase

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace authorization_service.Data;
+
+public class DataContext : IdentityDbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+}
