51c622759611575a627a8aef2e36f03093bc12da
firuz-macbook <razackoff@gmail.com>
2024-02-28 18:27:21 +0200

    Add repository.

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -3,8 +3,8 @@ using authorization_service.Models;
 namespace authorization_service.Repositories;
 
 public interface IUserRepository {
-    Task<User> GetByIdAsync(int id);
+    Task<User> GetByIdAsync(string id);
+    Task<User> GetByEmailAsync(string email);
     Task<User> AddAsync(User user);
-    Task UpdateAsync(User user);
     Task DeleteAsync(string id);
 }
