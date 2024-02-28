084d31a227ba9c65283909964716030580e3ec41
firuz-macbook <razackoff@gmail.com>
2024-02-28 14:46:36 +0200

    Add data context.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using authorization_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace authorization_service.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
