e3b23b4180e8e89cf8da361d80aa0fea92d51d00
firuz-macbook <razackoff@gmail.com>
2024-03-03 23:15:55 +0200

    Work version

--- obj/Debug/net8.0/authorization-service.AssemblyInfo.cs
+++ obj/Debug/net8.0/authorization-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("authorization-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e22443a05c13a08de47617a4d93f6cdb2d97bad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428c2b5013baf42ea7343bc2d89c1550208db4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("authorization-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("authorization-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
